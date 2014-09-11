016cc73d2caf634350af4664318b20999b643e73
josephyao <dermott@126.com>
2014-09-11 22:22:52 +0800

    Simplify code to use Stack

--- src/ParkingLot.java
+++ src/ParkingLot.java
@@ -1,33 +1,34 @@
+import java.util.Stack;
+
 public class ParkingLot {
 
-	private final int[] leaved;
-	private int leaved_pos = 0;
+    private final Stack<Integer> parkingSeats = new Stack<>();
+    private final int capacity;
 
 	public ParkingLot(int cap) {
 		if (cap <= 0) {
 			throw new IllegalArgumentException();
 		}
-		leaved = new int[cap];
-		do {
-			leaved[leaved_pos] = cap - leaved_pos;
-		} while (++leaved_pos < cap);
+        this.capacity = cap;
+        while (cap > 0)
+            parkingSeats.push(cap--);
 	}
 
 	public boolean isFull() {
-		return 0 == leaved_pos;
+		return parkingSeats.isEmpty();
 	}
 
 	public int park() {
-		if (leaved_pos > 0) {
-			return leaved[--leaved_pos];
+		if (!parkingSeats.isEmpty()) {
+            return parkingSeats.pop();
 		}
 		throw new IllegalStateException();
 	}
 
 	public void leave(int c) {
-		if (c <= 0 || c > leaved.length) {
+		if (c <= 0 || c > capacity) {
 			throw new IllegalArgumentException();
 		}
-		leaved[leaved_pos++] = c;
+        parkingSeats.push(c);
 	}
 }
